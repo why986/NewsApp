7eef8e4e1eb6710785b5bf0d4e2c0f6b2ddf038d
why986 <986496569@qq.com>
2020-09-06 01:22:09 +0800

    从接口获取数据以及存储到本地数据库

--- app/src/main/java/com/java/wanghaoyu/MainActivity.java
+++ app/src/main/java/com/java/wanghaoyu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.java.wanghaoyu;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.java.wanghaoyu.R;
@@ -14,7 +15,18 @@ import android.view.View;
 
 import com.java.wanghaoyu.ui.main.SectionsPagerAdapter;
 
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static Manager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +46,85 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        manager = Manager.getInstance(this);
+
     }
+
+/*
+    private static class Task extends AsyncTask<Void, Void, Task.Result> {
+        String url;
+        String type;
+        int page;
+        int size;
+
+        Task(String url, String type, int page, int size)
+        {
+            this.url = url;
+            this.type = type;
+            this.page = page;
+            this.size = size;
+        }
+        @Override
+        protected Result doInBackground(Void... voids) {
+            try {
+                URL url = new URL(this.url + "?type=" + this.type + "&page=" + this.page + "size=" + this.size);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setReadTimeout(3000);
+                httpURLConnection.setConnectTimeout(3000);
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), StandardCharsets.UTF_8));
+                StringBuilder stringBuilder = new StringBuilder();
+                String str;
+                while((str = in.readLine()) != null){
+                    stringBuilder.append(str);
+                    stringBuilder.append("\n");
+                }
+                in.close();
+                httpURLConnection.disconnect();
+
+                return new Result(STATE.OK, stringBuilder.toString());
+            }catch (SocketTimeoutException e) {
+                return new Result(STATE.TIMEOUT, null);
+            }catch (Exception e){
+                return new Result(STATE.OTHERS, null);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Result result) {
+            super.onPostExecute(result);
+            switch (result.state){
+                case OK:
+                    try {
+                        manager.dataBase.insertNewsList(type, page, result.data);
+                    }catch (JSONException e){
+
+                    }
+                    break;
+                case TIMEOUT:
+                    break;
+                case OTHERS:
+                    break;
+            }
+        }
+
+        enum STATE{OK, TIMEOUT, OTHERS}
+
+        class Result {
+            STATE state;
+            String data;
+
+            Result(STATE state, String data)
+            {
+                this.state = state;
+                this.data = data;
+            }
+        }
+    }
+
+ */
 }
--- app/src/main/java/com/java/wanghaoyu/Manager.java
+++ app/src/main/java/com/java/wanghaoyu/Manager.java
@@ -1,8 +1,22 @@
 package com.java.wanghaoyu;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Manager {
     private static Manager managerInstance = null;
@@ -19,8 +33,41 @@ public class Manager {
         return managerInstance;
     }
 
+    public static MyDataBase dataBase;
+
     private Manager(Context context) throws IOException
     {
+        this.dataBase = new MyDataBase(context);
+    }
 
+    List<SimpleNews> getSimpleNewsList(final String type, final int page, int size) throws JSONException
+    {
+        String url = "https://covid-dashboard.aminer.cn/api/events/list"
+                + "?type=" + type + "&page=" + page + "size=" + size;
+        OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder().url(url).get().build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d("GetSimpleNewsList", " Failed ");
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                try {
+                    dataBase.insertNewsList(type, page, response.body().string());
+                }catch (JSONException e)
+                {
+
+                }
+            }
+        });
+        return dataBase.getListSimpleNews(type, page);
+    }
+
+    DetailedNews getDetailedNews(String id) throws JSONException
+    {
+        return dataBase.getDetailedNews(id);
     }
 }
