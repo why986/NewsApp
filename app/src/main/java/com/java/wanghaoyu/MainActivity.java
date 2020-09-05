625b6535e7d9b34e5f2706f087f8b99b4b74300b
wmhst7 <wmhst@qq.com>
2020-09-05 21:46:18 +0800

    tabs&search

--- app/src/main/java/com/java/wanghaoyu/MainActivity.java
+++ app/src/main/java/com/java/wanghaoyu/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
