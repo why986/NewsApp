24fee3b008a1865898b138f830ad874b1ba1025b
wmhst7 <wmhst@qq.com>
2020-09-07 21:20:22 +0800

    实现TAB动态修改

--- app/src/main/java/com/java/wanghaoyu/MainActivity.java
+++ app/src/main/java/com/java/wanghaoyu/MainActivity.java
@@ -1,10 +1,15 @@
 package com.java.wanghaoyu;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -22,27 +27,94 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static Manager manager;
+    private Toolbar mtbar;
+    private ViewPagerAdapter adapter;
+    private ViewPager viewpager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewpager = (ViewPager) findViewById(R.id.viewpager);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewpager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewpager);
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        String type = "news";
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        initMain();
+    }
+
+    private void initMain(){
+        String type = "all";
+        adapter.addFragment(new NewsListFragment(type), type);
+        type = "news";
         adapter.addFragment(new NewsListFragment(type), type);
         type = "paper";
         adapter.addFragment(new NewsListFragment(type), type);
-//        type = "event";
-//        adapter.addFragment(new NewsListFragment(type), type);
-//        type = "啊这";
-//        adapter.addFragment(new NewsListFragment(type), type);
+
         viewpager.setAdapter(adapter);
+        mtbar = (Toolbar) findViewById(R.id.toolbar);
+        initToolBar();
+
+    }
 
 
+    private void initToolBar(){
+        setSupportActionBar(mtbar);
+//        //设置NavigationIcon的点击事件,需要放在setSupportActionBar之后设置才会生效,
+//        //因为setSupportActionBar里面也会setNavigationOnClickListener
+//        mtbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View pView) {
+//                // TODO: 2017/5/5 添加抽屉
+//
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String type;
+        switch (item.getItemId()) {
+            case R.id.menu_item_minus:
+                Toast.makeText(MainActivity.this, "种类--", Toast.LENGTH_LONG).show();
+                adapter.delFragment();
+
+                break;
+            case R.id.menu_item_add_paper:
+                Toast.makeText(MainActivity.this, "种类++", Toast.LENGTH_LONG).show();
+                type = "paper";
+                adapter.addFragment(new NewsListFragment(type), type);
+                break;
+            case R.id.menu_item_add_all:
+                Toast.makeText(MainActivity.this, "种类++", Toast.LENGTH_LONG).show();
+                type = "all";
+                adapter.addFragment(new NewsListFragment(type), type);
+                break;
+            case R.id.menu_item_add_points:
+                Toast.makeText(MainActivity.this, "种类++", Toast.LENGTH_LONG).show();
+                type = "points";
+                adapter.addFragment(new NewsListFragment(type), type);
+                break;
+            case R.id.menu_item_add_event:
+                Toast.makeText(MainActivity.this, "种类++", Toast.LENGTH_LONG).show();
+                type = "events";
+                adapter.addFragment(new NewsListFragment(type), type);
+                break;
+            case R.id.menu_item_add_news:
+                Toast.makeText(MainActivity.this, "种类++", Toast.LENGTH_LONG).show();
+                type = "news";
+                adapter.addFragment(new NewsListFragment(type), type);
+                break;
+        }
+        return true;
     }
 
 
