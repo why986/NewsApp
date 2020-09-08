ff062e2933cac4c42e32f136b1df9268f50d8749
wmhst7 <wmhst@qq.com>
2020-09-08 17:25:22 +0800

    merge,待完成：search，详情

--- app/src/main/java/com/java/wanghaoyu/ui/main/NewsListFragment.java
+++ app/src/main/java/com/java/wanghaoyu/ui/main/NewsListFragment.java
@@ -121,36 +121,6 @@ public class NewsListFragment extends Fragment implements SwipeRefreshLayout.OnR
             }
         }, type, page_count, SIZE);
     }
-
-
 }
 
-//public class NewsListFragment extends Fragment {
-//
-//    String type;
-//    List<SimpleNews> news_list;
-//    public NewsListFragment(String type){
-//        this.type = type;
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View view =  inflater.inflate(R.layout.new_list_fragment, container, false);
-//        Manager manager = Manager.getInstance(view.getContext());
-//        List<SimpleNews> news_list = manager.getSimpleNewsList(type, 1, 20);
-//
-//        System.out.println("SIMPLE NEW SIZE:  "+news_list.size());
-//
-//        NewsItemAdapter newsItemAdapter = new NewsItemAdapter(getActivity(), news_list);
-//        ListView listView = (ListView) view.findViewById(R.id.news_list_view);
-//        listView.setAdapter(newsItemAdapter);
-//        newsItemAdapter.notifyDataSetChanged();
-//
-//        return view;
-//    }
-//
-//
-//
-//}
+
