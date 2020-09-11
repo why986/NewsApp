8c10c736592cf070f7fe830ed6435c0415a224f2
wmhst7 <wmhst@qq.com>
2020-09-11 20:18:37 +0800

    不知道commit乐啥

--- app/src/main/java/com/java/wanghaoyu/MyDBOpenHelper.java
+++ app/src/main/java/com/java/wanghaoyu/MyDBOpenHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDBOpenHelper extends SQLiteOpenHelper {
     public MyDBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
-                          int version) {super(context, "mydatabase.db", null, 1); }
+                          int version) {super(context, "release.db", null, 1); }
     @Override
     //数据库第一次创建时被调用
     public void onCreate(SQLiteDatabase db) {
